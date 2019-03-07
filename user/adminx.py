647e9b8eb73583885e5ec21f7cbf379111e4b992
Sndav Bai <bossstyle@126.com>
2019-03-07 16:16:45 +0800

    Add Link Model & Add Redirect Page

--- user/adminx.py
+++ user/adminx.py
@@ -7,12 +7,6 @@ from .models import *
 from base.adminxBase import XadminBase
 
 
-class PublishedCourseAdmin(XadminBase):
-    list_display = ['user', 'publish']
-    search_fields = ['user', 'publish']
-    list_filter = ['user']
-
-
 class BaseSetting(XadminBase):  # 开启主题功能
     enable_themes = True
     use_bootswatch = True
@@ -28,5 +22,4 @@ class GlobalSettings(XadminBase):  # 全局修改，固定写法
 
 
 # 注册模块
-xadmin.site.register(PublishedCourse, PublishedCourseAdmin)
 xadmin.site.register(views.CommAdminView, GlobalSettings)
